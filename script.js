521057df075f1079eef16409191226883252b708
ayaladk <ayalagustavo855@gmail.com>
2025-07-21 17:36:40 -0300

    Big script refactor and minor changes to main index
    - Script file got a big refactor and now it's more clean and scalable. Old code from the previous lesson was removed and refactored for this new one.
    - Minor changes on the button text content to make it work.

--- script.js
+++ script.js
@@ -2,124 +2,30 @@
 
 function getComputerChoice() {
     let randomChoice = Math.random();
-    if (randomChoice >= 0 && randomChoice <= 0.33) {
-        return "rock";
-    }
+    if (randomChoice < 0.34) return "rock";
+    else if (randomChoice < 0.67) return "paper";
+    else return "scissors";
+}
 
-    else if (randomChoice >= 0.33 && randomChoice <= 0.66) {
-        return "paper";
-    }
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let buttonTextContent = button.textContent;
+        choiceButton(buttonTextContent)
+    });
+});
 
+function choiceButton(choice) {
+    let computerChoice = getComputerChoice();
+    if (choice === "rock" && computerChoice === "scissors" ||
+        choice === "paper" && computerChoice === "rock" ||
+        choice === "scissors" && computerChoice === "paper") {
+        console.log("You won.");
+    }
+    else if (choice == computerChoice) {
+        console.log("Draw.");
+    }
     else {
-        return "scissors";
-    }
-}
-
-/* Based on user's input, returns a string.*/
-
-function getHumanChoice() {
-    let choice = prompt("Let's play ROCK (R), PAPER (P), SCISSORS (S): ").toLowerCase();
-
-    if (choice === "R" || choice === "r" || choice === "rock") {
-        return "rock";
-    }
-
-    else if (choice === "P" || choice === "p" || choice === "paper") {
-        return "paper";
-    }
-
-    else if (choice === 'S' || choice === "s" || choice === "scissors") {
-        return "scissors";
-    }
-}
-
-/* Main game function. */
-
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    let draw = 0
-
-    function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    console.log(`You chose: ${humanChoice}`);
-    console.log(`Opponent chose: ${computerChoice}`);
-
-    if (humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You won.");
-            humanScore++;
-        }
-    else if (humanChoice === computerChoice) {
-        console.log("Draw.");
-        draw++;
-    } else {
-        console.log("You lost.");
-        computerScore++;
-    }
-    }
-
-    let i = 0
-    while (i <= 4) {
-        playRound();
-        i++;
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`\n- You won the game. Your score: ${humanScore}`);
-        console.log(`- Opponent's score: ${computerScore}`);
-        console.log(`- Draw: ${draw}`);
-    }
-    else if (humanScore < computerScore) {
-        console.log(`\n- You lost. Your score: ${humanScore}`);
-        console.log(`- Opponent's score: ${computerScore}`);
-        console.log(`- Draw: ${draw}`);
-    } else {
-        console.log("\n- Round draw. -\n");
-    }
-}
-
-const rock = document.querySelector(".rock");
-rock.addEventListener("click", () => {
-    let humanChoice = 'rock';
-    let computerChoice = getComputerChoice();
-
-    if (computerChoice == 'scissors') {
-        console.log("You won.");
-    } else if (computerChoice == 'paper') {
         console.log("You lose.");
-    } else {
-        console.log("Draw.");
     }
-});
-
-const paper = document.querySelector(".paper");
-paper.addEventListener("click", () => {
-    let humanChoice = 'paper';
-    let computerChoice = getComputerChoice();
-
-    if (computerChoice == 'rock') {
-        console.log("You won.");
-    } else if (computerChoice == 'scissors') {
-        console.log("You lose");
-    } else {
-        console.log("Draw.");
-    }
-});
-
-const scissors = document.querySelector(".scissors");
-scissors.addEventListener("click", () => {
-    let humanChoice = 'scissors';
-    let computerChoice = getComputerChoice();
-
-    if (computerChoice == 'paper') {
-        console.log("You won.");
-    } else if (computerChoice == 'rock') {
-        console.log("You lose");
-    } else {
-        console.log("Draw.");
-    }
-});
+}
